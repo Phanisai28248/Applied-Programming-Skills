11ae91e7fcbb165b018d2887a1082757c36d91e1
Phanisai28248 <vtu28248@veltech.edu.in>
2026-09-08 14:40:35 +0530

    Time: 27 ms (86.42%), Space: 54.6 MB (54.82%) - LeetHub

---
+++ 0901-online-stock-span/0901-online-stock-span.java
@@ -0,0 +1,22 @@
+import java.util.Stack;
+
+class StockSpanner {
+    // Stack stores pairs: int[]{price, span}
+    private Stack<int[]> stack;
+
+    public StockSpanner() {
+        stack = new Stack<>();
+    }
+    
+    public int next(int price) {
+        int span = 1;
+        
+        // Pop smaller or equal prices and aggregate their spans
+        while (!stack.isEmpty() && stack.peek()[0] <= price) {
+            span += stack.pop()[1];
+        }
+        
+        stack.push(new int[]{price, span});
+        return span;
+    }
+}
